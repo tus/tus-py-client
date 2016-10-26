bb9ad976152335bea6f7cdf09b41e85131395b4d
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2016-10-26 10:52:02 +0100

    docs: update doc with newly added features

--- tusclient/uploader.py
+++ tusclient/uploader.py
@@ -18,6 +18,10 @@ class Uploader(object):
         - file_path (str):
             This is the path(absolute/relative) to the file that is intended for upload
             to the tus server. On instantiation this attribute is required.
+        - file_stream (file):
+            As an alternative to the `file_path`, an insance of the file to be uploaded
+            can be passed to the constructor as `file_stream`. Do note that either the
+            `file_stream` or the `file_path` must be passed on instantiation.
         -  url (str):
             If the upload url for the file is known, it can be passed to the constructor.
             This may happen when you resume an upload.
@@ -29,10 +33,11 @@ class Uploader(object):
             This tells the uploader what chunk size(in bytes) should be uploaded when the
             method `upload_chunk` is called. This defaults to 2 * 1024 * 1024 i.e 2kb if not
             specified.
+        - metadata (dict):
+            A dictionary containing the upload-metadata. This would be encoded internally
+            by the method `encode_metadata` to conform with the tus protocol.
         - offset (int):
             The offset value of the upload indicates the current position of the file upload.
-        - file_size (int):
-            The size of the file.
         - stop_at (int):
             At what offset value the upload should stop.
         - request (<tusclient.request.TusRequest>):
@@ -40,9 +45,11 @@ class Uploader(object):
 
     :Constructor Args:
         - file_path (str)
+        - file_stream (Optional[file])
         - url (Optional[str])
         - client (Optional [<tusclient.client.TusClient>])
         - chunk_size (Optional[int])
+        - metadata (Optional[dict])
     """
     DEFAULT_HEADERS = {"Tus-Resumable": "1.0.0"}
     DEFAULT_CHUNK_SIZE = 2 * 1024 * 1024  # 2kb
@@ -100,6 +107,9 @@ class Uploader(object):
         return int(offset)
 
     def encode_metadata(self):
+        """
+        Return list of encoded metadata as defined by the Tus protocol.
+        """
         encoded_list = []
         for key, value in iteritems(self.metadata):
             key_str = str(key)  # dict keys may be of any object type.
@@ -148,6 +158,9 @@ class Uploader(object):
             raise TusUploadFailed
 
     def get_file_stream(self):
+        """
+        Return a file stream instance of the upload.
+        """
         if self.file_stream:
             self.file_stream.seek(0)
             return self.file_stream
@@ -158,6 +171,9 @@ class Uploader(object):
 
     @property
     def file_size(self):
+        """
+        Return size of the file.
+        """
         stream = self.get_file_stream()
         stream.seek(0, os.SEEK_END)
         return stream.tell()
