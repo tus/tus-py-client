532c56952f6c83115a004daa386e5f6f0b03c255
Mariusz Obajtek <mariusz.obajtek@screen9.com>
2017-04-12 19:13:07 +0200

    Add message to exception

--- tusclient/uploader.py
+++ tusclient/uploader.py
@@ -155,7 +155,7 @@ class Uploader(object):
         if self.request.status_code == 204:
             return True
         else:
-            raise TusUploadFailed
+            raise TusUploadFailed(self.request.return_value)
 
     def get_file_stream(self):
         """
