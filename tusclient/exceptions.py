9c605cecd0e509246c63d7c5db2a52bae7ec266b
Mariusz Obajtek <mariusz.obajtek@screen9.com>
2017-04-13 16:01:03 +0200

    Added documentation

--- tusclient/exceptions.py
+++ tusclient/exceptions.py
@@ -4,6 +4,21 @@ Global Tusclient exception and warning classes.
 
 
 class TusException(Exception):
+    """
+    Base exception class for Tus-related errors
+
+    :Attributes:
+        - message (str):
+            Main message of the exception
+        - status_code (int):
+            Status code of response indicating an error
+        - response_content (str):
+            Content of response indicating an error
+    :Constructor Args:
+        - message (Optional[str])
+        - status_code (Optional[int])
+        - response_content (Optional[str])
+    """
     def __init__(self, message='', status_code=400, response_content=''):
         super(TusException, self).__init__(message)
         self.status_code = status_code
