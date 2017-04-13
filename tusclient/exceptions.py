90a42444153e640eb573a33e242c3b006a716ca5
Mariusz Obajtek <mariusz.obajtek@screen9.com>
2017-04-13 15:32:14 +0200

    Pass more information in exceptions

--- tusclient/exceptions.py
+++ tusclient/exceptions.py
@@ -3,12 +3,19 @@ Global Tusclient exception and warning classes.
 """
 
 
-class TusUploadFailed(Exception):
+class TusException(Exception):
+    def __init__(self, message='', status_code=400, response_content=''):
+        super(TusException, self).__init__(message)
+        self.status_code = status_code
+        self.response_content = response_content
+
+
+class TusUploadFailed(TusException):
     """Should be raised when an attempted upload fails"""
     pass
 
 
-class TusCommunicationError(Exception):
+class TusCommunicationError(TusException):
     """
     Should be raised when communications with tus-server behaves
     unexpectedly.
