455333507fe84d3938c61f83ea781ecc5a7119be
Mariusz Obajtek <mariusz.obajtek@screen9.com>
2017-05-09 16:18:58 +0200

    Updating exception class hierarchy

--- tusclient/exceptions.py
+++ tusclient/exceptions.py
@@ -6,6 +6,12 @@ Global Tusclient exception and warning classes.
 class TusException(Exception):
     """
     Base exception class for Tus-related errors
+    """
+
+class TusCommunicationError(TusException):
+    """
+    Should be raised when communications with tus-server behaves
+    unexpectedly.
 
     :Attributes:
         - message (str):
@@ -19,20 +25,12 @@ class TusException(Exception):
         - status_code (Optional[int])
         - response_content (Optional[str])
     """
-    def __init__(self, message='', status_code=400, response_content=''):
+    def __init__(self, message, status_code, response_content):
         super(TusException, self).__init__(message)
         self.status_code = status_code
         self.response_content = response_content
 
 
-class TusUploadFailed(TusException):
+class TusUploadFailed(TusCommunicationError):
     """Should be raised when an attempted upload fails"""
-    pass
-
-
-class TusCommunicationError(TusException):
-    """
-    Should be raised when communications with tus-server behaves
-    unexpectedly.
-    """
     pass
