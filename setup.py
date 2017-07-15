52d50cb0f7a54560ebbd07fc4a003096c9711dc6
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-07-15 04:27:57 +0100

    docs: add long description to pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from setuptools import setup
 
@@ -10,6 +11,11 @@ if on_rtd:
 else:
     install_requires = ['pycurl==7.43.0', 'requests==2.11.1', 'six==1.10.0']
 
+PY_VERSION = sys.version_info[0], sys.version_info[1]
+if PY_VERSION < (3, 0):
+    long_description = open('README.md').read()
+else:
+    long_description = open('README.md', encoding='utf-8').read()
 
 setup(
     name='tuspy',
@@ -20,6 +26,7 @@ setup(
     install_requires=install_requires,
     author_email='ifedapoolarewaju@gmail.com',
     description='A Python client for the tus resumable upload protocol ->  http://tus.io',
+    long_description=(long_description),
     packages=['tusclient'],
     include_package_data=True,
     platforms='any',
