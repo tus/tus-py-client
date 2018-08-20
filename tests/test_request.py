711020a41dc0c727449e26dada33151f2876f13f
mochic808 <chrism@alleninstitute.org>
2018-08-20 11:22:15 -0700

    add support for tus upload-checksum header

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,4 +1,6 @@
 import mock
+import base64
+import hashlib
 
 from tusclient import client, request
 from tests import mixin
@@ -10,7 +12,6 @@ class TusRequestTest(mixin.Mixin):
         self.request = request.TusRequest(self.uploader)
 
     def test_perform(self):
-
         with mock.patch.object(self.request, 'handle') as mock_:
             self.request.handle = mock_
             self.request.perform()
@@ -23,6 +24,25 @@ class TusRequestTest(mixin.Mixin):
             mock_.request.assert_called_with(
                 'PATCH', '/files/15acd89eabdf5738ffc',
                 f.read(), headers)
+
+    def test_perform_checksum(self):
+        self.uploader.upload_checksum = True
+        self.request_checksum = request.TusRequest(self.uploader)
+        with mock.patch.object(self.request_checksum, 'handle') as mock_:
+            self.request_checksum.handle = mock_
+            self.request_checksum.perform()
+        with open('LICENSE', 'rb') as f:
+            license = f.read()
+            headers = {
+                'upload-offset': 0,
+                'Content-Type': 'application/offset+octet-stream'
+            }
+            headers.update(self.uploader.headers)
+            headers["upload-checksum"] = "sha1 " + \
+                base64.standard_b64encode(hashlib.sha1(license).digest()).decode("ascii")
+            mock_.request.assert_called_with(
+                'PATCH', '/files/15acd89eabdf5738ffc',
+                license, headers)
 
     def test_close(self):
         with mock.patch.object(self.request, 'handle') as mock_:
