be73ec26a6e09dcc6cf0f2142992b7fb021d1a7e
Adrian Partl <adrian.partl.ext@siemens.com>
2019-07-16 13:11:46 +0200

    migrated http.client lib to requests in request.py to enable proxy support

--- tusclient/request.py
+++ tusclient/request.py
@@ -1,7 +1,6 @@
-import http.client
 import errno
 import base64
-from future.moves.urllib.parse import urlparse
+import requests
 
 from tusclient.exceptions import TusUploadFailed
 
@@ -16,19 +15,15 @@ class TusRequest(object):
     on instantiation.
 
     :Attributes:
-        - handle (<http.client.HTTPConnection>)
+        - handle (<requests.Session>)
         - response_headers (dict)
         - file (file):
             The file that is being uploaded.
     """
 
     def __init__(self, uploader):
-        url = urlparse(uploader.url)
-        if url.scheme == 'https':
-            self.handle = http.client.HTTPSConnection(url.hostname, url.port)
-        else:
-            self.handle = http.client.HTTPConnection(url.hostname, url.port)
-        self._url = url
+        self.handle = requests.Session()
+        self._url = uploader.url
 
         self.response_headers = {}
         self.status_code = None
@@ -36,7 +31,7 @@ class TusRequest(object):
         self.file.seek(uploader.offset)
 
         self._request_headers = {
-            'upload-offset': uploader.offset,
+            'upload-offset': str(uploader.offset),
             'Content-Type': 'application/offset+octet-stream'
         }
         self._request_headers.update(uploader.headers)
@@ -51,16 +46,13 @@ class TusRequest(object):
         """
         Return response data
         """
-        return self._response.read()
+        return self._response.content
 
     def perform(self):
         """
         Perform actual request.
         """
         try:
-            host = '{}://{}'.format(self._url.scheme, self._url.netloc)
-            path = self._url.geturl().replace(host, '', 1)
-
             chunk = self.file.read(self._content_length)
             if self._upload_checksum:
                 self._request_headers["upload-checksum"] = \
@@ -70,18 +62,11 @@ class TusRequest(object):
                             self._checksum_algorithm(chunk).digest()
                         ).decode("ascii"),
                     ))
-            self.handle.request("PATCH", path, chunk, self._request_headers)
-            self._response = self.handle.getresponse()
-            self.status_code = self._response.status
-            self.response_headers = {k.lower(): v for k, v in self._response.getheaders()}
-        except http.client.HTTPException as e:
+            self._response = self.handle.patch(self._url, data=chunk, headers=self._request_headers)
+            self.status_code = self._response.status_code
+            self.response_headers = {k.lower(): v for k, v in self._response.headers.items()}
+        except requests.exceptions.RequestException as e:
             raise TusUploadFailed(e)
-        # wrap connection related errors not raised by the http.client.HTTP(S)Connection
-        # as TusUploadFailed exceptions to enable retries
-        except OSError as e:
-            if e.errno in (errno.EPIPE, errno.ESHUTDOWN, errno.ECONNABORTED, errno.ECONNREFUSED, errno.ECONNRESET):
-                raise TusUploadFailed(e)
-            raise e
 
     def close(self):
         """
