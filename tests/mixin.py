6d428119ff0db8740475bc22b0ad899c5491aeba
Marius <marius@transloadit.com>
2023-12-13 12:52:51 +0100

    Add explicit test fixtures to fix tests on Windows (#91)
    Co-authored-by: Nicholas Hairs <info@nicholashairs.com>

--- tests/mixin.py
+++ tests/mixin.py
@@ -4,6 +4,7 @@ import responses
 
 from tusclient import client
 
+FILEPATH_TEXT = "tests/sample_files/text.txt"
 
 class Mixin(unittest.TestCase):
     @responses.activate
@@ -12,4 +13,4 @@ class Mixin(unittest.TestCase):
         self.url = 'http://tusd.tusdemo.net/files/15acd89eabdf5738ffc'
         responses.add(responses.HEAD, self.url,
                       adding_headers={"upload-offset": "0"})
-        self.uploader = self.client.uploader('./LICENSE', url=self.url)
+        self.uploader = self.client.uploader(FILEPATH_TEXT, url=self.url)
